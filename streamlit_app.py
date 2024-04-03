3b22038a261b65ed73c10e442daeb59aafdb2e9b
Elaine M Paily <121116723+elainejpai-14@users.noreply.github.com>
2024-04-03 22:47:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,6 @@ import requests
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 
-# Set memory growth for GPU
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
-
 # Install dependencies from requirements.txt file
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
 
